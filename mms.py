845ac5c88bcaf833348c9b6614d7c0245dbba1d2
AleX-1337 <90471968+YT-AleX-1337@users.noreply.github.com>
2025-03-28 17:49:43 +0100

    Update mms.py

--- mms.py
+++ mms.py
@@ -353,7 +353,7 @@ while 1:
                 continue
             if t == 1:
                 print('\nMatrix type: Successor Ordinal')
-                print('\nPredecessor: ' + mat_to_string(clean(expand(m, 0), 1)))
+                print('\nPredecessor: ', mat_to_string(clean(expand(m, 0), 1)))
                 continue
             if t == 2:
                 print('\nMatrix type: Limit Ordinal')
